4ba516ea941425d5522935c86bed2a03624179aa
Sérgio Ernane Munhós Hermes <hermes.sergio@gmail.com>
2018-02-19 16:16:05 -0300

    added services

---
+++ src/NotafacilAPI/Image/Service/Factory/RequestedImageFactory.php
@@ -0,0 +1,36 @@
+<?php
+namespace NotafacilAPI\Image\Service\Factory;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use NotafacilAPI\Image\Entity\Image;
+
+/**
+ * Get Authenticated User Entity
+ *
+ * @author Sergio Hermes <hermes.sergio@gmail.com>
+ */
+class RequestedImageFactory implements FactoryInterface
+{
+    /**
+     * Create a service for Authenticated User
+     *
+     * @see \Zend\ServiceManager\FactoryInterface::createService()
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $router  = $serviceLocator->get('Router');
+        $request = $serviceLocator->get('Request');
+        $routeMatch  = $router->match($request);
+        $imageId = $routeMatch->getParam('image_id');
+        $requestedEntity = null;
+        if ($imageId === null) {
+            $requestedEntity = new Image();
+        } else {
+            $imageMapper = $serviceLocator->get('NotafacilAPI\\Image\\Mapper\\Image');
+            $requestedEntity = $imageMapper->fetchOne($imageId);
+        }
+        
+        return $requestedEntity;
+    }
+}
